0c35b765cff25081c6837a2f34378b68d505d09d
j2noo <wlsdndml213@gmail.com>
2025-04-13 16:14:11 +0900

    init : 프로젝트 초기 세팅

---
+++ src/main/java/com/be/bus/BusSeatAlarmApplication.java
@@ -0,0 +1,13 @@
+package com.be.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusSeatAlarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusSeatAlarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be/bus/BusSeatAlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusSeatAlarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
