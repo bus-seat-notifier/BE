5339eb9761e9dcc6f0541d8946d02e9f12a96cef
j2noo <wlsdndml213@gmail.com>
2025-07-13 16:27:31 +0900

    feat : 스케줄러 스레드풀 추가

---
+++ src/main/java/com/be/bus/global/config/SchedulerConfig.java
@@ -0,0 +1,19 @@
+package com.be.bus.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5); // 동시에 돌 수 있는 스케줄러 수
+        scheduler.setThreadNamePrefix("scheduler-");
+        return scheduler;
+    }
+}
